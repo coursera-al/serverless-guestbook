03f997e06d2593949dfeed761d01a9769a0bcdba
coursera-al <82797061+coursera-al@users.noreply.github.com>
2021-06-10 20:22:27 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d90e7830.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
